4049d5f0479f5c8e0891ca69d54a342584bcf2b7
Mohammad Anas <mohdanas406261@gmail.com>
2025-09-18 13:46:53 +0530

    New js code

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-let number = prompt("Enter a number: ");
-
-let check = (number % 5 === 0) ? " is a multiple of 5" : " is not a multiple of 5";
-
-console.log(number + check)
+let score = prompt("Enter your score");
+let grade;
+if (score <= 100 && score >= 80){
+    grade = "A";
+}else if (score < 80 && score >= 70){
+    grade = "B";
+}else if (score < 70 && score >= 60){
+    grade = "C";
+}else if (score < 60 && score >= 50){
+    grade = "D";
+}else if (score < 50 && score >= 0){
+    grade = "Fail";
+}else{
+    grade = "Enter number between 0 to 100";
+}
+console.log( "Grade " + grade);
